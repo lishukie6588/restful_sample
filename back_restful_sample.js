fc222e5eddf6bd27ac41d50d0339692a53032f2e
Shukie Li <59763257+lishukie1988@users.noreply.github.com>
2023-03-29 13:20:52 -0400

    1:20am 3/29/2023

--- back_restful_sample.js
+++ back_restful_sample.js
@@ -13,8 +13,9 @@ module.exports = function(){
 
     router.get('/get_car', function(req, res){
 
-        const car_specs = {type : req.query.car_type, turbo: req.query.car_turbo, fog_lights: req.query.car_fog_lights, heated_seats: req.query.car_heated_seats, autopilot: req.query.car_autopilot, sports_exhaust: req.query.car_sports_exhaust};
-        const returned_car_json = JSON.stringify(car_specs);
+        const car_specifications = {type : req.query.car_type, turbo: req.query.car_turbo, fog_lights: req.query.car_fog_lights, heated_seats: req.query.car_heated_seats, autopilot: req.query.car_autopilot, sports_exhaust: req.query.car_sports_exhaust};
+        const car = {car_object : car_specifications}
+        const returned_car_json = JSON.stringify(car);
 
         res.send(returned_car_json);
     
